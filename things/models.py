5dc70e00cc9cfc024be6a93fba57b913536720e2
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2022-10-06 11:13:01 +0100

    Define constructor for Thing model

--- things/models.py
+++ things/models.py
@@ -7,3 +7,8 @@ class Thing(models.Model):
     name = models.TextField
     description = models.TextField
     quantity = models.IntegerField
+
+    def __init__(self, name, description, quantity):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
