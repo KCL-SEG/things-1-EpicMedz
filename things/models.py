4a019112611a7995e2a4f883d43973ed2db17ca0
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2022-10-07 19:12:02 +0100

    2nd attempt at enforce input validation constraints on Things model

--- things/models.py
+++ things/models.py
@@ -6,8 +6,8 @@ from django.core.validators import MaxValueValidator
 # Create your models here.
 
 class Thing(Model):
-    name = models.CharField(unique = True, max_length = 30, blank = False)
-    description = models.CharField(unique = False, max_length = 120, blank = True )
+    name = models.CharField(max_length = 30)
+    description = models.CharField(max_length = 120, blank = False)
     quantity = models.IntegerField(unique = False, validators = [
         MinValueValidator(0, "Value cannot be less than 0"),
         MaxValueValidator(101, "Value cannot be greater than 100"
