d41c3230594ec2ce30156daacf12373bfbbf9ff5
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2022-10-09 00:30:12 +0100

    Solve migration errors and refine Thing model

--- things/models.py
+++ things/models.py
@@ -6,9 +6,9 @@ from django.core.validators import MaxValueValidator
 # Create your models here.
 
 class Thing(Model):
-    name = models.CharField(max_length = 30)
-    description = models.CharField(max_length = 120, blank = False)
-    quantity = models.IntegerField(unique = False, validators = [
+    name = models.CharField(max_length = 30, unique = True, blank = False)
+    description = models.CharField(max_length = 120, blank = True)
+    quantity = models.IntegerField(unique = False, default = 1, validators = [
         MinValueValidator(0, "Value cannot be less than 0"),
-        MaxValueValidator(101, "Value cannot be greater than 100"
+        MaxValueValidator(100, "Value cannot be greater than 100"
         )])
